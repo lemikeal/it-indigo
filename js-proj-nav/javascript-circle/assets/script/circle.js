23ea4ad642655eb8f8f5b488c9bec5a3f14f3a31
lemikeal <lemikeal@ualr.edu>
2026-02-12 18:43:02 +0000

    +Parks/Tweaks

---
+++ js-proj-nav/javascript-circle/assets/script/circle.js
@@ -0,0 +1,51 @@
+"use strict";
+
+$("#CircleForm").validate({});
+
+function displayCircle() {
+  if ($("#CircleForm").valid()) {
+    document.getElementById("diameter").innerHTML = "";
+    document.getElementById("circumference").innerHTML = "";
+    document.getElementById("area").innerHTML = "";
+
+    let radius;
+    let radiusfp;
+    let diameter;
+    let circumference;
+    let area;
+    let result;
+
+    radius = document.getElementById("radius").value;
+
+    radiusfp = parseFloat(radiusfp);
+
+    diameter = calcDiameter(radiusfp);
+    circumference = calcCircumference(radiusfp);
+    area = calcArea(radiusfp);
+
+    document.getElementById("diameter").innerHTML = diameter.toString();
+    document.getElementById("circumference").innerHTML =
+      circumference.toString();
+    document.getElementById("area").innerHTML = area.toString();
+  }
+}
+
+function calcDiameter(radiusvalue) {
+  return 2 * radiusvalue;
+}
+
+function calcCircumference(radiusvalue) {
+  return 2 * Math.PI * radiusvalue;
+}
+
+function calcArea(radiusvalue) {
+  return Math.PI * (radiusvalue * radiusvalue);
+}
+
+function clearForm() {
+  document.getElementById("radius").value = "";
+  document.getElementById("radiuserror").innerHTML = "";
+  document.getElementById("diameter").innerHTML = "";
+  document.getElementById("circumference").innerHTML = "";
+  document.getElementById("area").innerHTML = "";
+}
