4721b7670841cf30a57da60f63e839515049d65c
lemikeal <lemikeal@ualr.edu>
2026-02-19 18:23:31 +0000

    Parks Start

--- js-proj-nav/javascript-calculator/assets/script/calculator.js
+++ js-proj-nav/javascript-calculator/assets/script/calculator.js
@@ -1,17 +1,13 @@
 function calculate() {
     "use strict";
-    /* Make sure that the form is valid */
     if ($( "#myform" ).valid()) {
         
-        /* get the operands from the form */
         let operand1 = document.getElementById("Operand1").value;
         let operand2 = document.getElementById("Operand2").value;
         
-        /* convert the operands from string to floating point */
         let operand1fp = parseFloat (operand1);
         let operand2fp = parseFloat (operand2);
     
-        /* figure out which operator was checked and place the value in operator */
         let operator;
         if (document.getElementById("AddOperator").checked) {
             operator = document.getElementById("AddOperator").value;
@@ -28,34 +24,28 @@ function calculate() {
 
         let result;
         
-        /* if the operator was "Add" then add */
         if (operator == "Add") {
             result = (operand1fp + operand2fp);
         }
  
-        /* if the operator was "Sub" then subtract */
         if (operator == "Sub") {
             result = (operand1fp - operand2fp);
         }
 
-        /* if operator was "Mul" then multiply */
         if (operator == "Mul") {
             result = (operand1fp * operand2fp);
         }
 
-          /* if operator was "Div" then divide */
         if (operator == "Div") {
             result = (operand1fp / operand2fp);
         }
         
-        /* convert the result to a string and display it */
         document.getElementById("Result").innerHTML = result.toString();
     }
 }
 
 function clearform() {
     
-    /* Set all of the form values to blank or false */
     document.getElementById("Operand1").value = "";
     document.getElementById("Operand2").value = "";
     document.getElementById("Operand1Error").innerHTML = "";
@@ -68,7 +58,6 @@ function clearform() {
     document.getElementById("Result").innerHTML = "";
 }
 
-/* Form Validation */
 $( "#myform" ).validate({
  
 });
